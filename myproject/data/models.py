1ff2b5f68ba3a958c4a23ecd392f062040d7a539
lekdu <eduardo.cabezas.p@gmail.com>
2020-10-12 15:36:31 -0500

    Base models required

--- myproject/data/models.py
+++ myproject/data/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    
+class Review(models.Model):
+    rating = models.IntegerField()
+    title = models.CharField(max_length=64)
+    summary = models.CharField(max_length=10000)
+    ipAddress = models.CharField(max_length=15)
+    date = models.DateField(default=datetime.date.today)
+    company = models.OneToOneField(Company, on_delete=models.CASCADE)
+    reviewer = models.OneToOneField(User, on_delete=models.CASCADE)
+    
